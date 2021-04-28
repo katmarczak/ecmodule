9e9edab94432e1a8c59ad31c769e6dd9ea8bcc87
Katarzyna Nęcka <katarzyna.necka@gmail.com>
2021-04-28 16:24:38 +0200

    implemented pretty clunky but working 2d version

--- effective_conductivity.py
+++ effective_conductivity.py
@@ -2,30 +2,40 @@ import numpy as np
 from scipy import linalg
 
 def ec(tri, constants, r, cell):
-    n = int(len(tri.points) / 27)
+    base_n = 3**(len(cell.shape))
+    n = int(len(tri.points) / base_n)
     summ, value = 0, 0
     indptr, indices = tri.vertex_neighbor_vertices
+    _periodic_points = tri.points
     non_periodic_points = tri.points[0:n]
+
     for point_index in range(n):
         neighbours_indices = indices[indptr[point_index]:indptr[point_index+1]]
         for neighbour_index in neighbours_indices:
+            periodic_shift = _periodic_points[neighbour_index] - non_periodic_points[neighbour_index % n]
             p1 = constants[point_index]
-            p2 = constants[neighbour_index % n]
-            value = _g_mk(non_periodic_points[point_index], tri.points[neighbour_index], r, cell) * (p1 - p2)**2
+            p2 = constants[neighbour_index % n] + periodic_shift[0]
+            value = g_mk(non_periodic_points[point_index], tri.points[neighbour_index], r, cell) * (p1 - p2)**2
             summ += value
     return summ/2
 
 def min_ec(tri, r, cell):
-    gs, B = _construct_coefficients(tri, r, cell)
-    cons = _find_minima(gs, B)
+    gs, B = construct_coefficients(tri, r, cell)
+    cons = find_minima(gs, B)
     return ec(tri, cons, r, cell)
 
-def _g_mk(dm, dk, r, cell):
+def g_mk(dm, dk, r, cell):
     delta_mk = cell.distance(dm, dk) - 2*r
-    return -np.pi * r * np.log(delta_mk)
+    if len(cell.shape) == 2:
+        return np.pi * np.sqrt(r/delta_mk)
+    if len(cell.shape) == 3:
+        return -np.pi * r * np.log(delta_mk)
+    return None
 
-def _construct_coefficients(tri, r, cell):
-    n = int(len(tri.points) / 27)
+def construct_coefficients(tri, r, cell):
+    base_n = 3**(len(cell.shape))
+    n = int(len(tri.points) / base_n)
+    print('N of base points: %d, n of periodic points: %d' % (n, len(tri.points)))
     g_s = np.zeros((n, n))
     g_s[n-1][n-1] = 1
     vec_b = np.zeros(n)
@@ -37,7 +47,7 @@ def _construct_coefficients(tri, r, cell):
         neighbours_indices = indices[indptr[point_index]:indptr[point_index+1]]
         bsum = 0
         for neighbour_index in neighbours_indices:
-            g = _g_mk(non_periodic_points[point_index], tri.points[neighbour_index], r, cell)
+            g = g_mk(non_periodic_points[point_index], tri.points[neighbour_index], r, cell)
             g_s[point_index][point_index] += g
             g_s[point_index][neighbour_index % n] = -g
             factor = _periodic_points[neighbour_index] - non_periodic_points[neighbour_index % n]
@@ -46,5 +56,5 @@ def _construct_coefficients(tri, r, cell):
     
     return g_s, vec_b
 
-def _find_minima(M, b):
+def find_minima(M, b):
     return linalg.solve(M, b, check_finite=False, overwrite_a=True, overwrite_b=True)
--- geometry.py
+++ geometry.py
@@ -12,6 +12,21 @@ class _PeriodicCell:
     def random_point(self):
         pass
 
+class PeriodicCell2D(_PeriodicCell):    
+    def __init__(self, vecs: List[int]):
+        super()
+        self.x = vecs[0]
+        self.y = vecs[1]
+        self.shape = np.array([self.x, self.y])
+    
+    def random_point(self):
+        return np.array([np.random.random() * self.x, np.random.random() * self.y])
+    
+    def distance(self, p1, p2):
+        d = p1 - p2
+        periodic_d = np.minimum(np.abs(d), self.shape - np.abs(d))
+        return np.linalg.norm(periodic_d)
+
 class PeriodicCell3D(_PeriodicCell):    
     def __init__(self, vecs: List[int]):
         super()
@@ -58,7 +73,7 @@ def rsa3d(cell, n, conc = None):
             A.append(sphere_new)
     return np.array(A), radius
 
-def _periodize3d(points, cell):
+def periodize3d(points, cell):
     periodic_points = []
     lx, ly, lz = cell.shape
     
@@ -72,10 +87,32 @@ def _periodize3d(points, cell):
                 shift = [x, y, z]
                 periodic_points.append(np.add(points, shift))
                                 
-    return np.array(periodic_points)
+    return np.array(periodic_points) # shape: 27 x n_of_points x 3
+
+def periodize2d(points, cell):
+    periodic_points = []
+    lx, ly = cell.shape
+    
+    xs = [0, -lx, lx]
+    ys = [0, -ly, ly]
+    
+    for x in xs:
+        for y in ys:
+            shift = [x, y]
+            periodic_points.append(np.add(points, shift))
+                                
+    return np.array(periodic_points) # shape: 9 x n_of_points x 2 
 
 def make_delaunay(points, cell):
-    per_points = _periodize3d(points, cell).reshape(270, 3)
-    per_points = np.column_stack((per_points[:, 0], per_points[:, 1], per_points[:, 2]))
+    '''make periodic delaunay from non-periodic points''' 
+    per_points = None
+    n = len(points)
+    if len(cell.shape) == 2:
+        per_points = periodize2d(points, cell).reshape(9*n, 2)
+        per_points = np.column_stack((per_points[:, 0], per_points[:, 1]))
+    if len(cell.shape) == 3:
+        per_points = periodize3d(points, cell).reshape(27*n, 3)
+        per_points = np.column_stack((per_points[:, 0], per_points[:, 1], per_points[:, 2]))
+    
     tri = Delaunay(per_points, qhull_options='Qc')
     return tri
