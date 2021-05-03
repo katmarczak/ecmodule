3b1e493aababa4ae186bde08040d86005ab42b5f
Katarzyna Nęcka <katarzyna.necka@gmail.com>
2021-05-03 19:42:46 +0200

    implemented a case of different radii

--- effective_conductivity.py
+++ effective_conductivity.py
@@ -1,21 +1,45 @@
 import numpy as np
 from scipy import linalg
 
+# tri + r + cell jako jeden obiekt?
+
+def gmk_factory(r, cell):
+    g_mk = None
+    dm = cell.shape.size
+    if dm == 2:
+        if np.all(np.isclose(r, r[0])): # equal_radii
+            def g_mk(dm, dk, r, cell):
+                r = r[0]
+                delta_mk = cell.distance(dm, dk) - 2*r
+                return np.pi * np.sqrt(r/delta_mk)
+        else:
+            def g_mk(dm, dk, r, cell):
+                delta_mk = cell.distance(dm, dk) - (r[0]+r[1])
+                return (np.pi / np.sqrt(delta_mk)) * np.sqrt( (2*r[0]*r[1]) / (r[0]+r[1]) )
+    if dm == 3:
+        def g_mk(dm, dk, r, cell):
+            delta_mk = cell.distance(dm, dk) - 2*r
+            return np.pi * np.sqrt(r/delta_mk)
+    
+    return g_mk
+
 def ec(tri, constants, r, cell):
-    base_n = 3**(len(cell.shape))
+    base_n = 3**(cell.shape.size)
     n = int(len(tri.points) / base_n)
     summ, value = 0, 0
     indptr, indices = tri.vertex_neighbor_vertices
     _periodic_points = tri.points
     non_periodic_points = tri.points[0:n]
 
+    g_mk = gmk_factory(r, cell)
+
     for point_index in range(n):
         neighbours_indices = indices[indptr[point_index]:indptr[point_index+1]]
         for neighbour_index in neighbours_indices:
             periodic_shift = _periodic_points[neighbour_index] - non_periodic_points[neighbour_index % n]
-            p1 = constants[point_index]
-            p2 = constants[neighbour_index % n] + periodic_shift[0]
-            value = g_mk(non_periodic_points[point_index], tri.points[neighbour_index], r, cell) * (p1 - p2)**2
+            p1, p2 = constants[point_index], constants[neighbour_index % n] + periodic_shift[0]
+            radii = r[point_index], r[neighbour_index % n]
+            value = g_mk(non_periodic_points[point_index], tri.points[neighbour_index], radii, cell) * (p1 - p2)**2
             summ += value
     return summ/2
 
@@ -24,18 +48,19 @@ def min_ec(tri, r, cell):
     cons = find_minima(gs, B)
     return ec(tri, cons, r, cell)
 
-def g_mk(dm, dk, r, cell):
-    delta_mk = cell.distance(dm, dk) - 2*r
-    if len(cell.shape) == 2:
-        return np.pi * np.sqrt(r/delta_mk)
-    if len(cell.shape) == 3:
-        return -np.pi * r * np.log(delta_mk)
-    return None
+# def g_mk(dm, dk, r, cell): # r powinno byc zawsze para wartosci ale moze byc r1=r2
+#     delta_mk = cell.distance(dm, dk) - 2*r # -(r1+r2)
+#     if len(cell.shape) == 2:
+#         return np.pi * np.sqrt(r/delta_mk)
+#     if len(cell.shape) == 3:
+#         return -np.pi * r * np.log(delta_mk)
+    
+#     return (np.pi/np.sqrt(delta_mk)) * np.sqrt(2*rk*rm/rk+rm)
+#     return None
 
 def construct_coefficients(tri, r, cell):
-    base_n = 3**(len(cell.shape))
+    base_n = 3**(cell.shape.size)
     n = int(len(tri.points) / base_n)
-    print('N of base points: %d, n of periodic points: %d' % (n, len(tri.points)))
     g_s = np.zeros((n, n))
     g_s[n-1][n-1] = 1
     vec_b = np.zeros(n)
@@ -43,11 +68,14 @@ def construct_coefficients(tri, r, cell):
     _periodic_points = tri.points
     non_periodic_points = tri.points[0:n]
 
+    g_mk = gmk_factory(r, cell)
+
     for point_index in range(n-1):
         neighbours_indices = indices[indptr[point_index]:indptr[point_index+1]]
         bsum = 0
         for neighbour_index in neighbours_indices:
-            g = g_mk(non_periodic_points[point_index], tri.points[neighbour_index], r, cell)
+            radii = r[point_index], r[neighbour_index % n]
+            g = g_mk(non_periodic_points[point_index], tri.points[neighbour_index], radii, cell)
             g_s[point_index][point_index] += g
             g_s[point_index][neighbour_index % n] = -g
             factor = _periodic_points[neighbour_index] - non_periodic_points[neighbour_index % n]
